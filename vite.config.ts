9f9580131aa132435e677bbd8bc28884becb2d65
Bhuvanesh <mbhuvanesh325@gmail.com>
2026-05-02 23:18:55 +0530

    fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // 🔥 MUST ADD
   plugins: [react()],
   build: {
     outDir: "dist",
